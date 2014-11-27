0c7a1f2956f45a51fbb0b2516a033d93168e83d5
Christoffer Pedersen <mail@cstp.dk>
2014-11-27 20:28:53 +0000

    added ssh to lein

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject do-coreos "0.1"
   :description "Coreos on digital ocean"
-  :plugins [[lein-exec "0.3.4"]]
   :dependencies [[digitalocean "1.2"] [org.clojure/clojure "1.5.1"]
-                 [clj-http "1.0.1"]]
+                 [clj-http "1.0.1"] [clj-ssh "0.5.11"]]
   :source-paths ["."]
   :clean-targets ^{:protect false} ["target"])
